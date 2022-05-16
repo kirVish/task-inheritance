ee09507d44f5397a96054d0b4981ecf7be0387d4
kirVish <prprchvish@gmail.com>
2022-05-16 17:44:47 +0300

    INHERITANCE

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -3,11 +3,9 @@ interface IPersonData {
     secondName: string;
     age: number;
 }
-
 interface IStudentData extends IPersonData {
     phone: string;
 }
-
 /*
 1. Напишите конструктор, который принимает в качестве параметра объект с интерфейсом IPersonData
 2. Добавьте метод getData(), возвращающий объект, соответствующий интерфейсу IPersonData
@@ -16,12 +14,35 @@ export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
+    constructor(obj: IPersonData) {
+        this._age = obj.age;
+        this._name = obj.name;
+        this._secondName = obj.secondName;
+    }
+    getData(): IPersonData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
-
 /*
 1. Напишите конструктор, который принимает в качестве параметра объект с интерфейсом IStudentData
 2. Добавьте метод getData(), возвращающий объект, соответствующий интерфейсу IStudentData
  */
 export class Student extends Person {
     readonly _phone: string;
+    constructor(obj: IStudentData) {
+        super(obj);
+        this._phone = obj.phone;
+    }
+    getData(): IStudentData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+            phone: this._phone,
+        };
+    }
 }
